34bdc8d49eded3b1e1e570bbdb7e6b5b6179e0b3
tonitagen <tiffany_215@163.com>
2016-12-29 11:45:29 +0800

    new class

---
+++ okhttp/src/main/java/com/example/tonitagen/okhttp/CustomActivity.java
@@ -0,0 +1,19 @@
+package com.example.tonitagen.okhttp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 55345364 on 2016/12/29.
+ */
+
+public class CustomActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
